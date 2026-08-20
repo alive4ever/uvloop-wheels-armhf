2ad0acf51f3fd98856e46c32e7a36164cba10377
alive4ever <alive4ever@users.noreply.github.com>
2026-08-20 15:26:29 +0000

    build: add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+set -e
+cd ~
+if [ -n "$PYTHON_VERSION" ] ; then
+	PYTHON="$PYTHON_VERSION"
+else
+	PYTHON="python3"
+fi
+echo "Using $PYTHON"
+TAG="v0.22.1"
+git clone --revision="$TAG" --recursive "https://github.com/MagicStack/uvloop"
+$PYTHON -m venv venv
+. ./venv/bin/activate
+pip install -U pip
+pip install build
+cd ./uvloop
+python -m build -w -o ../dist
+cd ..
